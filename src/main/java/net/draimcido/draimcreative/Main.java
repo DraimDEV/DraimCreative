c53750130e4d11203e7946b5e74f3e16e821463b
DraimDEV <danilaorlov4@gmail.com>
2022-08-07 17:28:39 +0300

    [1.0.0] Включение/Отключение дебага
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

--- src/main/java/net/draimcido/draimcreative/Main.java
+++ src/main/java/net/draimcido/draimcreative/Main.java
@@ -4,6 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    // Включение/Отключение дебага.
+    public static boolean DEBUG = false;
+    public static boolean EXTRADEBUG = false;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
