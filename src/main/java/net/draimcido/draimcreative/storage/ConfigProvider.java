c92bb863f4f5bcb51432254d43f55099647b7cfd
DraimDEV <danilaorlov4@gmail.com>
2022-08-07 17:32:50 +0300

    [1.0.0] Создание ConfigProvider
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

---
+++ src/main/java/net/draimcido/draimcreative/storage/ConfigProvider.java
@@ -0,0 +1,14 @@
+package net.draimcido.draimcreative.storage;
+
+import net.draimcido.draimcreative.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class ConfigProvider {
+    private Main main;
+    private FileConfiguration config;
+    private String name;
+
+    public ConfigProvider(Main main, String name) {
+
+    }
+}
