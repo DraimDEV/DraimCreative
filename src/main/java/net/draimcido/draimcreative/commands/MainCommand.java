4257b2c3306138946db76a12c3758d9ddc9c1867
DraimDEV <danilaorlov4@gmail.com>
2022-08-08 12:57:28 +0300

    [1.0.0] Создание /dcreative reload
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

--- src/main/java/net/draimcido/draimcreative/commands/MainCommand.java
+++ src/main/java/net/draimcido/draimcreative/commands/MainCommand.java
@@ -12,8 +12,25 @@ public class MainCommand implements CommandExecutor {
         this.main = pl;
     }
 
+    // Все команды плуга
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length == 0) return false;
+
+        switch (args[0].toLowerCase()) {
+            case "reload":
+                if (sender.hasPermission("dcreative.commads.reload")) {
+                    reload(sender);
+                    return true;
+                }
+                break;
+        }
+
+        return true;
+    }
+
+    // Метод перезагрузки плагина
+    public void reload(CommandSender sender) {
+        main.reloadConfigs();
     }
 }
