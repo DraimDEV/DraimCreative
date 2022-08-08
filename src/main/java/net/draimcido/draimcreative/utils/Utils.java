d470fb0b74e475b6545bd028a5d651f4dabdde87
DraimDEV <danilaorlov4@gmail.com>
2022-08-08 13:02:32 +0300

    [1.0.0] Создание Отправки сообщения
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

--- src/main/java/net/draimcido/draimcreative/utils/Utils.java
+++ src/main/java/net/draimcido/draimcreative/utils/Utils.java
@@ -1,4 +1,11 @@
 package net.draimcido.draimcreative.utils;
 
+import org.bukkit.command.CommandSender;
+
 public class Utils {
+
+    // Отправка сообщения
+    public static void sendMSG(CommandSender sender, String msg) {
+        if (!msg.equalsIgnoreCase("")) sender.sendMessage(msg);
+    }
 }
