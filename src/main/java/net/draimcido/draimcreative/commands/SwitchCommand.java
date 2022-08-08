ef63b02ff4bb5515bd25da3b9fd36a6cc6d43ef8
DraimDEV <danilaorlov4@gmail.com>
2022-08-08 13:00:36 +0300

    [1.0.0] Создание SwitchCommand
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

--- src/main/java/net/draimcido/draimcreative/commands/SwitchCommand.java
+++ src/main/java/net/draimcido/draimcreative/commands/SwitchCommand.java
@@ -1,4 +1,35 @@
 package net.draimcido.draimcreative.commands;
 
-public class SwitchCommand {
+import net.draimcido.draimcreative.Main;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SwitchCommand implements CommandExecutor {
+
+    private Main main;
+    private GameMode gm;
+
+    public SwitchCommand(Main pl, GameMode gm) {
+        this.main = pl;
+        this.gm = gm;
+    }
+
+    private Main getMain() {
+        return this.main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        switch (args.length) {
+            case 0:
+                if (!(sender instanceof Player)) return false;
+
+            Player p = (Player) sender;
+        }
+
+        return true;
+    }
 }
