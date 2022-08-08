95a156cf3809dea44fe481eee5aceffcfab63a2e
DraimDEV <danilaorlov4@gmail.com>
2022-08-08 13:03:02 +0300

    [1.0.0] Новый вид для "Отправки сообщения"
    Signed-off-by: DraimDEV <danilaorlov4@gmail.com>

--- src/main/java/net/draimcido/draimcreative/utils/Utils.java
+++ src/main/java/net/draimcido/draimcreative/utils/Utils.java
@@ -5,7 +5,5 @@ import org.bukkit.command.CommandSender;
 public class Utils {
 
     // Отправка сообщения
-    public static void sendMSG(CommandSender sender, String msg) {
-        if (!msg.equalsIgnoreCase("")) sender.sendMessage(msg);
-    }
+    public static void sendMSG(CommandSender sender, String msg) { if (!msg.equalsIgnoreCase("")) sender.sendMessage(msg); }
 }
